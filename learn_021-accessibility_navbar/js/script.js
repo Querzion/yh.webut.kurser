a7e86866c7e37f33b7bab04a11d05045c6bea8dc
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-09-17 20:37:55 +0200

    Navbar Lesson done.

---
+++ learn_021-accessibility_navbar/js/script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu-btn')
+const mainMenu = document.querySelector('#main-menu')
+
+
+menuBtn.addEventListener('click', () => {
+
+    const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true'
+    
+    if(isExpanded) {
+        menuBtn.setAttribute('aria-expanded', false)
+        mainMenu.addEventListener('animationend', () => {
+            mainMenu.classList.add('hide')
+        }, {once: true})
+    }
+    else {
+        mainMenu.classList.remove('hide')
+        menuBtn.setAttribute('aria-expanded', true)
+    }
+
+})
