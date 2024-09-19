c85bcd24882e1b3551f09ef41f24f4d975677788
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-09-20 01:00:51 +0200

    I created a Template folder, wwhich will expand over time.

---
+++ template/js/darkmode.js
@@ -0,0 +1,39 @@
+// För att det här scriptet ska fungera så behöver du lägga till en .dark klass i din css där du stylar dina färger
+// du behöver också en checkbox med id="darkmode-switch" i din HTML
+
+
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasDarkmode = localStorage.getItem('darkmode')
+
+if(hasDarkmode == null) {
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+} else if(hasDarkmode === 'on') {
+  enableDarkMode()
+} else if(hasDarkmode === 'off') {
+  disableDarkMode()
+}
+
+
+
+darkmodeSwitch.addEventListener('change', () => {
+  if(darkmodeSwitch.checked) {
+    enableDarkMode()
+    localStorage.setItem('darkmode', 'on')
+  } else {
+    disableDarkMode()
+    localStorage.setItem('darkmode', 'off')
+  }
+})
+
+function enableDarkMode() {
+  darkmodeSwitch.checked = true
+  document.documentElement.classList.add('dark')
+}
+function disableDarkMode() {
+  darkmodeSwitch.checked = false
+  document.documentElement.classList.remove('dark')
+}
