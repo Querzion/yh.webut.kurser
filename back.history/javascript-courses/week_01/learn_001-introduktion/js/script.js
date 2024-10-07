98cae134672e1d8dd97a2c1690cadbb529d93fd3
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-10-07 13:41:43 +0200

    changes in the structure

---
+++ back.history/javascript-courses/week_01/learn_001-introduktion/js/script.js
@@ -0,0 +1,26 @@
+//  Semicolon is not a must in javascript.
+
+console.log('Hello World')      //  User message through the console (basically console print or output 'hello world')
+console.log('Hello World');     //  User message through the console (basically console print or output 'hello world')
+console.log("Hello World")      //  User message through the console (basically console print or output "hello world")
+console.log("Hello World");     //  User message through the console (basically console print or output "hello world")
+
+console.warn("Warning");            //  Mostly a command to use in the situation that you want to state that something is wrong with your code, HERE.
+console.error("Error message");     //  Mostly a command to use in the situation that you want to state that something is wrong with your code, HERE.
+
+//  Inline comment
+
+/* 
+    This is a 
+
+
+    . . . 
+
+
+
+    Multi
+    line 
+    comment
+
+*/
+
