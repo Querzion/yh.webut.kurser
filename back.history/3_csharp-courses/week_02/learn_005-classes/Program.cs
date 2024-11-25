b1846470c63b28da2972ad214e46b6baa50f9da9
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-11-25 13:58:15 +0100

    Added User class, added tokengenerator class file.

--- back.history/3_csharp-courses/week_02/learn_005-classes/Program.cs
+++ back.history/3_csharp-courses/week_02/learn_005-classes/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///     PROPERTIES
+///     
+///     There are four definitions that you can give,
+///     a variable, method or class, and they work like this.
+///     
+
+///     Public
+///     
+//      Works Internally,
+//      between classes that are nested,
+//      between different classes in the same project,
+//      between different classes in the same solution.
+
+///     Internal
+///     
+//      Works internally,
+//      between classes that are nested,
+//      between different classes in the same project.
+
+///     Protected
+///     
+//      Works internally,
+//      between classes that are nested.
+
+///     Private
+///     
+//      Works internally.
+
+using learn_005_classes;
+
+User user = new User();
+
+var userTestCamp = new User()
+{
+    FirstName = "Dracul"
+};
+
+
+Console.Write("Gift me your name: ");
+user.FirstName = Console.ReadLine();
+Console.WriteLine(user.FirstName);
+
+if (user.FirstName == "Why?")
+{
+    user.FirstName = userTestCamp.FirstName;
+    Console.WriteLine("Is it " + user.FirstName + " then?");
+
+    user.FirstName = "Slisk";
+    Console.WriteLine("I know what you did last summer " + user.FirstName);
+}
+
+/// FIELDS
+/// 
+
+
+
+Console.ReadKey();
--- back.history/3_csharp-courses/week_02/learn_005-classes/obj/Debug/net9.0/learn_005-classes.AssemblyInfo.cs
+++ back.history/3_csharp-courses/week_02/learn_005-classes/obj/Debug/net9.0/learn_005-classes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d015a3179c0e7de459081d69f0d6168dbba7aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9aa3273dd182f565569abcef5b41533be5febc")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
