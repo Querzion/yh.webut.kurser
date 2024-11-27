0642ecf54d0a49f449f51a7a94dfa9549b3afee7
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-11-27 09:20:14 +0100

    exercise 03 project created, not to work with yet.

--- back.history/3_csharp-courses/week_01/learn_004-loops-for.foreach.while.etc/obj/Debug/net8.0/learn_004-loops-for.foreach.while.etc..AssemblyInfo.cs
+++ back.history/3_csharp-courses/week_01/learn_004-loops-for.foreach.while.etc/obj/Debug/net8.0/learn_004-loops-for.foreach.while.etc..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn_004-loops-for.foreach.while.etc.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590c9853eddfbeacd82e81515bf69582b83eb1fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bfc14d586772feeded1b3633c4e151b7abcb2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn_004-loops-for.foreach.while.etc.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn_004-loops-for.foreach.while.etc.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
