be51738dea2e0549ea17e2f18d82eea5ceda4384
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-11-27 16:42:08 +0100

    Added unnecessary text to the promt.

--- back.history/3_csharp-courses/week_02/learn_005-classes/obj/Debug/net9.0/learn_005-classes.AssemblyInfo.cs
+++ back.history/3_csharp-courses/week_02/learn_005-classes/obj/Debug/net9.0/learn_005-classes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8154b7ea2e7639aa38fab5d0f8ae0d13ac5f57ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cced09fafb5869339f715327a43020d73b21dafe")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn_005-classes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
