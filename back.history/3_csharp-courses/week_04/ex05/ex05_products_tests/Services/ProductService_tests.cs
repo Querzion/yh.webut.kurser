290f04b23d998d8690eae71864b63444c5202443
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-12-11 00:10:05 +0100

    update

--- back.history/3_csharp-courses/week_04/ex05/ex05_products_tests/Services/ProductService_tests.cs
+++
@@ -1,6 +0,0 @@
-namespace ex05_products_tests.Services;
-
-public class ProductServiceTests
-{
-    
-}
---
+++ back.history/3_csharp-courses/week_04/ex05/ex05_products_tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ex05_products_tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
