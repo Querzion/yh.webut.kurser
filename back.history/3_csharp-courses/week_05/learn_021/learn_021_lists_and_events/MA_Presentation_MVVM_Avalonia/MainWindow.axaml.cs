34b6db52cf91c8fd1f913af79172d883dbccfee8
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-12-20 16:09:57 +0100

    basebuild created.

---
+++ back.history/3_csharp-courses/week_05/learn_021/learn_021_lists_and_events/MA_Presentation_MVVM_Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MA_Presentation_MVVM_Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
