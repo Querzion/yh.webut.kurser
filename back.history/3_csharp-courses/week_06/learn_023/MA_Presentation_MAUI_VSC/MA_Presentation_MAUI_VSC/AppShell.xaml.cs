395c663ec9d921cb422c76f672da837eb58d8519
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-12-29 15:35:00 +0100

    Delete back.history/3_csharp-courses/week_06 directory

--- back.history/3_csharp-courses/week_06/learn_023/MA_Presentation_MAUI_VSC/MA_Presentation_MAUI_VSC/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MA_Presentation_MAUI_VSC;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- back.history/3_csharp-courses/week_06/learn_023/MA_Presentation_MAUI_VSC/MA_Presentation_MAUI_VSC/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MA_Presentation_MAUI_VSC;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
